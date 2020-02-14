b53b0f61c0f3ff262fca5bdfa1ea6cfa64d6deee
AlexKaravaev <karavaev-sasha@mail.ru>
2020-02-14 03:21:33 +0300

    Updated comparison code

--- scripts/make_video.py
+++ scripts/make_video.py
@@ -1,13 +1,13 @@
 # Prepare thresholded points for DBSCAN clustering algorithm
 from math import sqrt, ceil
-from sklearn.cluster import DBSCAN
+from sklearn.cluster import DBSCAN,KMeans
 import scipy as sp
 import hdbscan
 import copy
 import seaborn as sns
 import io
 import matplotlib.backend_bases as plt_base
-
+import time
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
@@ -84,7 +84,7 @@ def showHist(hist, figsize=(14, 5)):
     
     plt.show()
     
-def clusterize(input_img, color):
+def clusterize(input_img, color, algo, hd_avg_time, db_avg_time, k_avg_time, i):
     img = color
     thresh_input = copy.deepcopy(input_img)
     #color_input = cv2.cvtColor(thresh_input, cv2.COLOR_GRAY2BGR)
@@ -104,46 +104,130 @@ def clusterize(input_img, color):
     # Translate DBSCAN proximity threshold to units of pixels and run DBSCAN
     pixproxthr = proxthresh * sqrt(binimg.shape[0]**2 + binimg.shape[1]**2)
 
+    if algo == 0:
+        start = time.time()
+        hdb = hdbscan.HDBSCAN(min_cluster_size=500, min_samples=200).fit(Xslice)
+        hd_avg_time += time.time()-start
+        print(f"HDBSCAN mean time over {i} runs: {hd_avg_time/i}")
+        labels = hdb.labels_.astype(int)
+        
+        n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+        n_noise_ = list(labels).count(-1)
 
-    hdb = hdbscan.HDBSCAN(min_cluster_size=30,min_samples=200, cluster_selection_epsilon=pixproxthr, core_dist_n_jobs=-1).fit(Xslice)
-    labels = hdb.labels_.astype(int)
-    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-    n_noise_ = list(labels).count(-1)
-
-    # Find the largest cluster (i.e., with most points) and obtain convex hull   
-    unique_labels = set(labels)
-    core_samples_mask = np.zeros_like(hdb.labels_, dtype=bool)
+        # Find the largest cluster (i.e., with most points) and obtain convex hull   
+        unique_labels = set(labels)
+        core_samples_mask = np.zeros_like(hdb.labels_, dtype=bool)
   
-    #core_samples_mask[hdb.validity.internal_minimum_spanning_tree.internal_nodes] = True
-    colors = [plt.cm.Spectral(each)
-              for each in np.linspace(0, 1, len(unique_labels))]
-    fig = plt.figure(frameon=False)
+        #core_samples_mask[hdb.validity.internal_minimum_spanning_tree.internal_nodes] = True
+        colors = [plt.cm.Spectral(each)
+                for each in np.linspace(0, 1, len(unique_labels))]
+        fig = plt.figure(frameon=False)
     
-    plot1 = fig.add_axes([0.,0.,1.,1.])
-    plot1.axis('off')
+        plot1 = fig.add_axes([0.,0.,1.,1.])
+        plot1.axis('off')
     
-    clusterer = hdb
-    color_palette = sns.color_palette('deep', 32)#len(np.unique(clusterer.labels_)))
-    if len(clusterer.labels_) == 0:
-        return img
-    #print(clusterer.labels_)
-    #print(len(clusterer.labels_))
-    prob_mask = clusterer.probabilities_ >= 1
-    print(prob_mask.shape)
-    print(clusterer.labels_.shape)
-    clusterer.labels_ = clusterer.labels_[prob_mask]
-    X = X[prob_mask]
+        clusterer = hdb
+        color_palette = sns.color_palette('deep', 32)#len(np.unique(clusterer.labels_)))
+        if len(clusterer.labels_) == 0:
+            return img
+        #print(clusterer.labels_)
+        #print(len(clusterer.labels_))
+        prob_mask = np.logical_and(clusterer.probabilities_ >= 0.75, clusterer.labels_ != -1)
 
-    cluster_colors = np.array([color_palette[x] if x >= 0
-                  else (0.5, 0.5, 0.5)
-                  for i, x in enumerate(clusterer.labels_)])
+        clusterer.labels_ = clusterer.labels_[prob_mask]
+        X = X[prob_mask]
+
+        cluster_colors = np.array([color_palette[x] if x >= 0
+                    else (0.5, 0.5, 0.5)
+                    for i, x in enumerate(clusterer.labels_)])
 
    
-    cluster_member_colors = [sns.desaturate(x, p)
-                             for x, p in
-                         zip(cluster_colors, clusterer.probabilities_)]
+        cluster_member_colors = [sns.desaturate(x, p)
+                                for x, p in
+                            zip(cluster_colors, clusterer.probabilities_)]
+    if algo == 1:
+        start = time.time()
+        db = DBSCAN(eps=10, min_samples=200).fit(X)
+        db_avg_time += time.time() - start
+        print(f"DBSCAN mean time over {i} runs: {db_avg_time/i}")
+        labels = db.labels_.astype(int)
+        n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+        n_noise_ = list(labels).count(-1)
+
+        # Find the largest cluster (i.e., with most points) and obtain convex hull   
+        unique_labels = set(labels)
+        core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+  
+        #core_samples_mask[hdb.validity.internal_minimum_spanning_tree.internal_nodes] = True
+        colors = [plt.cm.Spectral(each)
+                for each in np.linspace(0, 1, len(unique_labels))]
+        fig = plt.figure(frameon=False)
     
+        plot1 = fig.add_axes([0.,0.,1.,1.])
+        plot1.axis('off')
+
+        clusterer = db
+        color_palette = sns.color_palette('deep', 32)#len(np.unique(clusterer.labels_)))
+        if len(clusterer.labels_) == 0:
+            return img
+        #print(clusterer.labels_)
+        #print(len(clusterer.labels_))
+        clusterer.probabilities_ = np.ones(db.labels_.shape)
+        prob_mask = np.logical_and(clusterer.probabilities_ >= 1, clusterer.labels_ != -1)
+
+        clusterer.labels_ = clusterer.labels_[prob_mask]
+        X = X[prob_mask]
+
+        cluster_colors = np.array([color_palette[x] if x >= 0
+                    else (0.5, 0.5, 0.5)
+                    for i, x in enumerate(clusterer.labels_)])
+
+   
+        cluster_member_colors = [sns.desaturate(x, p)
+                                for x, p in
+                            zip(cluster_colors, clusterer.probabilities_)]
     
+    if algo == 2:
+        start = time.time()
+        db = KMeans(n_clusters=6, random_state=0).fit(X)
+        k_avg_time += time.time() - start
+        print(f"K-means time: {k_avg_time/i}")
+        labels = db.labels_.astype(int)
+        n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+        n_noise_ = list(labels).count(-1)
+
+        # Find the largest cluster (i.e., with most points) and obtain convex hull   
+        unique_labels = set(labels)
+        core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+  
+        #core_samples_mask[hdb.validity.internal_minimum_spanning_tree.internal_nodes] = True
+        colors = [plt.cm.Spectral(each)
+                for each in np.linspace(0, 1, len(unique_labels))]
+        fig = plt.figure(frameon=False)
+    
+        plot1 = fig.add_axes([0.,0.,1.,1.])
+        plot1.axis('off')
+
+        clusterer = db
+        color_palette = sns.color_palette('deep', 32)#len(np.unique(clusterer.labels_)))
+        if len(clusterer.labels_) == 0:
+            return img
+        #print(clusterer.labels_)
+        #print(len(clusterer.labels_))
+        clusterer.probabilities_ = np.ones(db.labels_.shape)
+        prob_mask = np.logical_and(clusterer.probabilities_ >= 1, clusterer.labels_ != -1)
+
+        clusterer.labels_ = clusterer.labels_[prob_mask]
+        X = X[prob_mask]
+
+        cluster_colors = np.array([color_palette[x] if x >= 0
+                    else (0.5, 0.5, 0.5)
+                    for i, x in enumerate(clusterer.labels_)])
+
+   
+        cluster_member_colors = [sns.desaturate(x, p)
+                                for x, p in
+                            zip(cluster_colors, clusterer.probabilities_)]
     
     plot1.scatter(X[:,1],X[:,0], s=50, linewidth=0, c=cluster_member_colors, alpha=1)
 
@@ -177,14 +261,19 @@ def makeVideo(filename, output):
     cap = cv2.VideoCapture( filename + ".avi" )
     ret, frame = cap.read()
 
-    scale = 0.3
+    scale = 0.8
     size = 1920, 1080
     res_l = cv2.VideoWriter(output + "_l.avi" ,fourcc, 20.0, size)
     res_b = cv2.VideoWriter(output + "_b.avi" ,fourcc, 20.0, size)
     res_otsu = cv2.VideoWriter(output + "_o.avi" ,fourcc, 20.0, size)
     res_mask = cv2.VideoWriter(output + "_m.avi" ,fourcc, 20.0, size)
-    
+    i = 0 
+    hd_avg_time = 0
+    db_avg_time = 0
+    k_avg_time  = 0
     while cap.isOpened():
+        i += 1
+
         ret, frame = cap.read()
         if not ret:
             break
@@ -218,17 +307,42 @@ def makeVideo(filename, output):
         road = cv2.resize(road, None, fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
 
 
-        mask = clusterize(clr_thresh, road)
+        #start = time.time()
+        mask = clusterize(clr_thresh, road, 0, hd_avg_time, db_avg_time, k_avg_time, i)
+        #hd_avg_time += time.time()-start
+        #print(f"HDBSCAN mean time over {i} runs: {hd_avg_time/i}")
+        
         mask = cv2.resize(mask, (road.shape[1], road.shape[0]))
         
         white = cv2.cvtColor(white, cv2.COLOR_GRAY2BGR)
 
+        if i % 5 == 0:
+            cv2.imwrite('../images/paper_images/thresh_{}.jpg'.format(i), thresh_hel)
+            cv2.imwrite('../images/paper_images/clr_thresh_{}.jpg'.format(i), clr_thresh)
+            cv2.imwrite('../images/paper_images/cluster_{}_hdb.jpg'.format(i), mask)
+            cv2.imwrite('../images/paper_images/orig_{}.jpg'.format(i), road)
+
+        #start = time.time()
+        mask = clusterize(clr_thresh, road, 1, hd_avg_time, db_avg_time, k_avg_time, i)
+        #db_avg_time += time.time() - start
+        #print(f"DBSCAN mean time over {i} runs: {db_avg_time/i}")
+        mask = cv2.resize(mask, (road.shape[1], road.shape[0]))
         
+        if i % 5 == 0:
+            cv2.imwrite('../images/paper_images/cluster_{}_db.jpg'.format(i), mask)
+        #start = time.time()
+        mask = clusterize(clr_thresh, road, 2, hd_avg_time, db_avg_time, k_avg_time, i)
+        #k_avg_time += time.time() - start
+        #print(f"K-means time: {k_avg_time/i}")
+        mask = cv2.resize(mask, (road.shape[1], road.shape[0]))
+        
+        if i % 5 == 0:
+            cv2.imwrite('../images/paper_images/cluster_{}_km.jpg'.format(i), mask)
         comb = np.hstack((clr_thresh, mask))
         cv2.imshow('win', comb)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
+        
         comb = cv2.resize(comb, size)
         white = cv2.resize(white, size)
         yellow = cv2.resize(yellow, size)
@@ -248,7 +362,7 @@ video_path_res = "../images/video/res/"
 video_path_clips = "../images/video/clips/"
 
 makeVideo(video_path_clips + 'IMG_0252', video_path_res + 'comb')
-makeVideo(video_path_clips + 'bright', video_path_res + 'bright')
-makeVideo(video_path_clips + 'cloudy', video_path_res + 'cloudy')
-makeVideo(video_path_clips + 'pale', video_path_res + 'pale')
+#makeVideo(video_path_clips + 'bright', video_path_res + 'bright')
+#makeVideo(video_path_clips + 'cloudy', video_path_res + 'cloudy')
+#makeVideo(video_path_clips + 'pale', video_path_res + 'pale')
 makeVideo(video_path_clips + 'dark', video_path_res + 'dark')
